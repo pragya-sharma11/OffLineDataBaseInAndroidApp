7fe49a59338692fd97b6379fadb88536c6d67cde
github <pragya.sharma2_cs19@gla.ac.in>
2021-11-01 00:13:59 +0530

    declare variables for login page.

--- app/src/main/java/com/example/offlinedatabaseinandroidapp/MainActivity.java
+++ app/src/main/java/com/example/offlinedatabaseinandroidapp/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.offlinedatabaseinandroidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button b1, b2;
+    EditText e1, e2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
