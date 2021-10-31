a2ed1a636e934f3ce1ef7bd93a8a708e1cf4d5dd
github <pragya.sharma2_cs19@gla.ac.in>
2021-11-01 00:04:33 +0530

    check if values are empty then give message otherwise create database.

--- app/src/main/java/com/example/offlinedatabaseinandroidapp/Signup.java
+++ app/src/main/java/com/example/offlinedatabaseinandroidapp/Signup.java
@@ -3,6 +3,7 @@ package com.example.offlinedatabaseinandroidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -28,6 +29,25 @@ public class Signup extends AppCompatActivity {
                 finish();
             }
         });
-
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s1 = e1.getText().toString();
+                String s2 = e2.getText().toString();
+                String s3 = e3.getText().toString();
+                if(s1.isEmpty()){
+                    e1.setError("please fill username");
+                }
+                else if (s2.isEmpty()){
+                    e2.setError("Please fill email address");
+                }
+                else if(s3.isEmpty()){
+                    e3.setError("Please enter password");
+                }
+                else{
+                    SQLiteDatabase database = openOrCreateDatabase("Authentication", MODE_PRIVATE, null );
+                }
+            }
+        });
     }
 }
