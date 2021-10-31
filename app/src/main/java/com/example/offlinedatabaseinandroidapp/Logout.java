48fa95299a5f7361a99810978714d0b0f28b32bf
github <pragya.sharma2_cs19@gla.ac.in>
2021-11-01 00:27:47 +0530

    make functionality for logout

--- app/src/main/java/com/example/offlinedatabaseinandroidapp/Logout.java
+++ app/src/main/java/com/example/offlinedatabaseinandroidapp/Logout.java
@@ -2,13 +2,25 @@ package com.example.offlinedatabaseinandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Logout extends AppCompatActivity {
-
+    Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logout2);
+        b = findViewById(R.id.button5);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Logout.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
