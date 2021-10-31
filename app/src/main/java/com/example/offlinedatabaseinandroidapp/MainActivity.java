a44fc841204c434db9453fa5a80aefea0e69450b
github <pragya.sharma2_cs19@gla.ac.in>
2021-11-01 00:16:46 +0530

    make functionality for register button on login page

--- app/src/main/java/com/example/offlinedatabaseinandroidapp/MainActivity.java
+++ app/src/main/java/com/example/offlinedatabaseinandroidapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.offlinedatabaseinandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -17,6 +19,13 @@ public class MainActivity extends AppCompatActivity {
         b2 = findViewById(R.id.button2);
         e1 = findViewById(R.id.editText1);
         e2 = findViewById(R.id.editText2);
-
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Signup.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
