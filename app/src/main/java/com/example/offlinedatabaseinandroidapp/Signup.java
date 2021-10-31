b1edfb1f3d8a1c829e7226597a22abdc5bd43a68
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-31 23:54:55 +0530

    declare variables for views in signup

--- app/src/main/java/com/example/offlinedatabaseinandroidapp/Signup.java
+++ app/src/main/java/com/example/offlinedatabaseinandroidapp/Signup.java
@@ -3,9 +3,12 @@ package com.example.offlinedatabaseinandroidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Signup extends AppCompatActivity {
-
+    Button b1, b2;
+    EditText e1, e2, e3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
